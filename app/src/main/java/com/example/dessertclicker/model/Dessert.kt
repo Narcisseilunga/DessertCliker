7d14f09164c268131d9a4b8daee6c43c7815e0c6
Narcisseilunga <ilunganarcisse353@gmail.com>
2024-05-24 09:57:21 +0200

    deuxième commit

--- app/src/main/java/com/example/dessertclicker/model/Dessert.kt
+++ app/src/main/java/com/example/dessertclicker/model/Dessert.kt
@@ -14,18 +14,9 @@
  * limitations under the License.
  */
 
-package com.example.dessertclicker.ui.theme
+package com.example.dessertclicker.model
 
-import androidx.compose.ui.graphics.Color
-
-val md_theme_light_primary = Color(0xFF006781)
-val md_theme_light_onPrimary = Color(0xFFFFFFFF)
-val md_theme_light_secondaryContainer = Color(0xFFCFE6F1)
-val md_theme_light_onSecondaryContainer = Color(0xFF071E26)
-val md_theme_light_background = Color(0xFFFBFCFE)
-
-val md_theme_dark_primary = Color(0xFF5FD4FD)
-val md_theme_dark_onPrimary = Color(0xFF003544)
-val md_theme_dark_secondaryContainer = Color(0xFF354A53)
-val md_theme_dark_onSecondaryContainer = Color(0xFFCFE6F1)
-val md_theme_dark_background = Color(0xFF191C1D)
+/**
+ * [Dessert] is the data class to represent the Dessert imageId, price, and startProductionAmount
+ */
+data class Dessert(val imageId: Int, val price: Int, val startProductionAmount: Int)
